559d2cb446fdb953c64ed6cc4f1eab94265b33f0
DSM3 <fatec-dsm3@academico.intra>
2025-03-18 19:41:21 -0300

    Atualizando Organização de Pastas | Adicionando Nova aplicação que usa mongo atlas

---
+++ api-the-games-mongo-db-atlas/routes/gameRoutes.js
@@ -0,0 +1,15 @@
+import express from "express";
+const gameRoutes = express.Router();
+import gameController from "../controllers/gameController.js";
+
+// Endpoint para listar todos os games (rota)
+gameRoutes.get("/games", gameController.getAllGames);
+// Endpoint para cadastrar novo jogo
+gameRoutes.post("/games", gameController.createGame);
+// Endpoint para deletar jogo pelo id
+// Se o param fosse opcional seria :id?
+gameRoutes.delete("/games/:id", gameController.deleteGame);
+// Endpoint para alterar um jogo
+gameRoutes.put("/games/:id", gameController.updateGame);
+
+export default gameRoutes;
---
+++ api-the-games-mongo-db-atlas/services/gameService.js
@@ -0,0 +1,72 @@
+import GameRepository from "../models/Games.js";
+
+class gameService {
+  // Game.find().then(games => {
+  // //sucesso
+  // }).catch(error => {
+  // // falha
+  // })
+
+  // async / await
+  async getAll() {
+    try {
+      const games = await GameRepository.find();
+      return games;
+    } catch (error) {
+      console.log(error);
+    }
+  }
+
+  //Função para cadastrar jogos
+  async create(title, year, price, descriptions) {
+    console.log("Teste Service");
+    try {
+      const newGame = new GameRepository({
+        //title : title
+        title,
+        year,
+        price,
+        descriptions,
+      });
+      //Método do mongoose para cadastrar .save()
+      await newGame.save();
+    } catch (error) {
+      console.log(error);
+    }
+  }
+
+  //Função para deletar jogos
+  async delete(id) {
+    try {
+      await GameRepository.findByIdAndDelete(id);
+      console.log(`Game Excluded - Id: ${id}}`);
+    } catch (error) {
+      console.log(error);
+    }
+  }
+
+  async update(id, title, year, price, descriptions) {
+    try {
+      await GameRepository.findByIdAndUpdate(id, {
+        //title = title
+        title,
+        year,
+        price,
+        descriptions,
+      });
+      console.log(`Dados do game atualizados com sucesso. ID = ${id}`);
+    } catch (error) {
+      console.log(error);
+    }
+  }
+  async getOne(id) {
+    try {
+      const game = await GameRepository.findOne({ _id: id });
+      return game;
+    } catch (error) {
+      console.log(error);
+    }
+  }
+}
+
+export default new gameService();
